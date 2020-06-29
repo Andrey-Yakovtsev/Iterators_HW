02ac7a3b58e98deb973a4a48f6416462a6e29ded
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-06-29 12:53:20 +0300

    Initial - working model

---
+++ Iterators.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import wikipediaapi
+from pprint import pprint
+
+
+def get_country_official_name():
+    with open('countries.json', encoding='utf-8') as countries_data:
+        country = json.load(countries_data)
+        countries_official_names_list = []
+        for item in country:
+            countries_official_names_list.append(item['name']['official'])
+        return countries_official_names_list
+
+
+
+wiki_wiki = wikipediaapi.Wikipedia('en')
+countries_links_list = []
+for country in get_country_official_name():
+    try:
+        page_py = wiki_wiki.page(country)
+        country_item = {'country': country,  'url': page_py.fullurl}
+    except KeyError:
+        country_item = {'country': country,  'url': 'No link'}
+    countries_links_list.append(country_item)
+
+with open('countries_links.json', 'w') as fi:
+    json.dump(countries_links_list, fi, ensure_ascii=False, indent=2)
+#
+# class WikiIterator:
+#
+#     def __init__(self, start, end):
+#         self.start = start
+#         self.end = end
+#         self.current = start - 1
+#         self.session = requests.session()
+#
+#     def __iter__(self, country):
+#         self.country = country
+#         return country_link
+#
+#     def __next__(self):
+
+
