fc11b17cb2b1d83e9fc019859bccb8d8c2b7c5c4
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-06-30 19:01:07 +0300

    rewriting to Class - new tries

--- Iterator_in_Class.py
+++ Iterator_in_Class.py
@@ -2,45 +2,41 @@ import json
 import wikipediaapi
 
 wiki_wiki = wikipediaapi.Wikipedia('en')
-# countries_links_list = []
-#
-# for country in get_country_official_name():
-#     try:
-#         page_py = wiki_wiki.page(country)
-#         country_item = {'country': country,  'url': page_py.fullurl}
-#     except KeyError:
-#         country_item = {'country': country,  'url': 'No link'}
-#
-#     countries_links_list.append(country_item)
 
+
+
+def get_country_official_name():
+    with open('countries.json', encoding='utf-8') as countries_data:
+        country = json.load(countries_data)
+        countries_official_names_list = []
+        for item in country:
+            countries_official_names_list.append(item['name']['official'])
+    return countries_official_names_list
 
 
 
 class WikiIterator:
 
-    def __init__(self, start=int, end=int):
+    def __init__(self, object, start=int, end=int):
         self.start = start
         self.end = end
         self.current = start - 1 #пока нигде не применяю
-
+        self.object = object
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        return self
-
-    def get_country_official_name(self, start, end):
-        with open('countries.json', encoding='utf-8') as countries_data:
-            country = json.load(countries_data)
-            countries_official_names_list = []
-            for i in range(start, end):
-                countries_official_names_list.append(country[i]['name']['official'])
-                return countries_official_names_list
+        self.start += 1
+        if self.start == self.end:
+            raise StopIteration
+        return self.start
 
 
 
-for item in WikiIterator.get_country_official_name(1, 10, 11):
+
+
+for item in WikiIterator(get_country_official_name()):
     print(item)
 '''
 в списке стран бежим по индексу
@@ -52,3 +48,5 @@ for item in WikiIterator.get_country_official_name(1, 10, 11):
 
     # with open('countries_links.json', 'w') as fi:
     #     json.dump(countries_links_list, fi, ensure_ascii=False, indent=2)
+
+# print(get_country_official_name())
